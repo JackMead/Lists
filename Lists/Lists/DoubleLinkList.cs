13fb33107d8e9f2f498a7c3b1743432b97927d84
JackMead <Jack.Mead@softwire.com>
2018-01-19 15:45:40 +0000

    Working array and double-linked lists

---
+++ Lists/Lists/DoubleLinkList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    public class DoubleLinkList
+    {
+        private int length;
+        private DoubleNode firstElement;
+        private DoubleNode lastElement;
+
+        public DoubleLinkList()
+        {
+            length = 0;
+        }
+
+        public void Add(string value)
+        {
+            var newNode = new DoubleNode(value, lastElement);
+            if (lastElement != null)
+            {
+                lastElement.NextNode = newNode;
+            }
+            lastElement = newNode;
+            length++;
+
+            if (length == 1)
+            {
+                firstElement = newNode;
+            }
+        }
+
+        public void Delete(string value)
+        {
+            length--;
+            var nodeToDelete = Find(value);
+            var previousNode = nodeToDelete.PreviousNode;
+            var nextNode = nodeToDelete.NextNode;
+            if (previousNode != null)
+            {
+                previousNode.NextNode = nextNode;
+            }
+            else
+            {
+                firstElement = nextNode;
+            }
+            if (nextNode != null)
+            {
+                nextNode.PreviousNode = previousNode;
+            }
+            else
+            {
+                lastElement = previousNode;
+            }
+        }
+
+        public DoubleNode Find(string value)
+        {
+            var node = firstElement;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (node.Value == value)
+                {
+                    return node;
+                }
+                node = node.NextNode;
+            }
+            return node;
+        }
+
+        public string[] Values()
+        {
+            var values = new string[length];
+            var node = firstElement;
+            for (int i = 0; i < length; i++)
+            {
+                values[i] = node.Value;
+                node = node.NextNode;
+            }
+            return values;
+        }
+    }
+
+    public class DoubleNode
+    {
+        public string Value { get; }
+        public DoubleNode NextNode { get; set; }
+        public DoubleNode PreviousNode { get; set; }
+
+        public DoubleNode(string value, DoubleNode previousNode)
+        {
+            Value = value;
+            PreviousNode = previousNode;
+            NextNode = null;
+        }
+    }
+
+}
---
+++ Lists/Lists/MyArrayList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    public class MyArrayList
+    {
+        private int length;
+        private string[] array;
+
+        public MyArrayList(int capacity)
+        {
+            length = 0;
+            array = new string[capacity];
+        }
+
+        public void Add(string value)
+        {
+            length++;
+            if (length <= array.Length)
+            {
+                array[length-1] = value;
+                return;
+            }
+            DoubleArraySize();
+            array[length-1] = value;
+        }
+
+        public void DoubleArraySize()
+        {
+            var currentLength = array.Length;
+            var newArray = new string[currentLength * 2];
+            CopyArrayIntoBiggerArray(array, newArray);
+            array = newArray;
+        }
+
+        public void CopyArrayIntoBiggerArray(string[] arr1, string[] arr2)
+        {
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr2[i] = arr1[i];
+            }
+        }
+
+        public void Delete(string value)
+        {
+            var index=FindPositionByValue(value);
+            for (int i = index; i < length-1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+                length--;
+        }
+
+        public int FindPositionByValue(string value)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if(array[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public string Find(string value)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (array[i]==value)
+                {
+                    return value;
+                }
+            }
+            return null;
+        }
+
+        public string[] Values()
+        {
+            return FirstNElements(array, length);
+        }
+
+        public string[] FirstNElements(string[] array, int length)
+        {
+            var returnArray = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                returnArray[i] = array[i];
+            }
+            return returnArray;
+        }
+    }
+}
